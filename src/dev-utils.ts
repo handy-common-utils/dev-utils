ce4a8d8f428c4dbc48d732fb3500f23d7b294c11
James Hu <james.hu@open.edu.au>
2020-11-01 18:09:18 +1100

    first checkin

---
+++ src/dev-utils.ts
@@ -0,0 +1,35 @@
+import { CliApplication } from 'typedoc';
+import concatMd from "concat-md";
+import { FsUtils } from '@handy-common-utils/fs-utils';
+
+const API_DOCS_DIR = 'api-docs';
+const README_MD_FILE = 'README.md';
+
+export abstract class DevUtils {
+  static async generateApiDocsMd(): Promise<void> {
+    const tdCli = new CliApplication();
+    tdCli.bootstrap({
+      out: API_DOCS_DIR,
+      mode: 'file',
+      readme: 'none',
+      plugin: ['typedoc-plugin-markdown'],
+      disableSources: true,
+      excludePrivate: true,
+    });
+  }
+
+  static async generateApiDocsAndUpdateReadme(): Promise<void> {
+    DevUtils.generateApiDocsMd();
+    const apiDocsContentPromise = concatMd(API_DOCS_DIR, {
+      toc: false,
+      decreaseTitleLevels: true,
+      dirNameAsTitle: true,
+      startTitleLevelAt: 2,
+    })
+    .then(content => content.replace(/`Static`\*\*/g, '`Static` **'))
+    .then(content => `<!-- API start -->${content}<!-- API end -->`);
+    await FsUtils.replaceContentInFile(README_MD_FILE, /<!-- API start -->[\s\S]*<!-- API end -->/m, () => apiDocsContentPromise);
+    await FsUtils.addSurrounding(README_MD_FILE, /\*\*`example`\*\*([\s\S]*?)###/gm, '**`example`**\n```javascript\n', '```\n###');
+  }
+
+}
---
+++ test/dev-utils.spec.ts
@@ -0,0 +1,7 @@
+import { expect } from 'chai';
+
+describe('DevUtils', () => {
+  it('dummy', () => {
+    expect(1).to.eq(1);
+  });
+});
