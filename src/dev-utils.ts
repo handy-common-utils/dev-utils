e976e7283a2e9b1ad2e6a8342050eaa2f9d51b27
James Hu <james.hu.ustc@hotmail.com>
2021-09-08 16:35:33 +1000

    update for accomodating latest typdoc

--- src/dev-utils.ts
+++ src/dev-utils.ts
@@ -1,4 +1,4 @@
-import { Application } from 'typedoc';
+import { Application, TypeDocReader, TSConfigReader } from 'typedoc';
 import concatMd from 'concat-md';
 import { FsUtils } from '@handy-common-utils/fs-utils';
 
@@ -7,15 +7,24 @@ const README_MD_FILE = 'README.md';
 
 export abstract class DevUtils {
   static async generateApiDocsMd(): Promise<void> {
-    const tdCli = new Application();
-    tdCli.bootstrap({
+    const app = new Application();
+    // app.options.addReader(new ArgumentsReader(0));
+    app.options.addReader(new TypeDocReader());
+    app.options.addReader(new TSConfigReader());
+    // tdCli.options.addReader(new ArgumentsReader(300));
+
+    app.bootstrap({
       entryPoints: ['./src'],
       out: API_DOCS_DIR,
       readme: 'none',
       plugin: ['typedoc-plugin-markdown'],
       disableSources: true,
       excludePrivate: true,
+      excludeExternals: true,
     });
+
+    const project = app.convert()!;
+    await app.generateDocs(project, API_DOCS_DIR);
   }
 
   static async generateApiDocsAndUpdateReadme(): Promise<void> {
