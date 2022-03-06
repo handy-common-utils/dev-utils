36e6f5325d570f5aab8ffe48a3def968ef7f189b
James Hu <james.hu.ustc@hotmail.com>
2022-03-06 15:16:27 +1100

    add test case

--- test/dev-utils.spec.ts
+++ test/dev-utils.spec.ts
@@ -1,7 +1,28 @@
 import { expect } from 'chai';
+import { DevUtils } from '../src/dev-utils';
+import * as fs from 'fs-extra';
+import * as path from 'node:path';
+import { FsUtils } from '@handy-common-utils/fs-utils';
 
+const TMP_DIR = 'tmp';
 describe('DevUtils', () => {
-  it('dummy', () => {
-    expect(1).to.eq(1);
+  it('should be able to generate api docs and update readme', async function () {
+    this.timeout(10000);
+
+    const PATTERN_STRING = '<!-- API start -->\nDUMMY\n<!-- API end -->';
+
+    fs.removeSync(TMP_DIR);
+    fs.mkdirSync(TMP_DIR);
+    const readmePath = path.join(TMP_DIR, 'README.md');
+    fs.copyFileSync('README.md', readmePath);
+    await FsUtils.replaceInFile(readmePath, /<!-- API start -->([\s\S]*)<!-- API end -->/m, PATTERN_STRING);
+    expect(fs.readFileSync(readmePath, 'utf8')).to.include(PATTERN_STRING);
+
+    await DevUtils.generateApiDocsAndUpdateReadme(readmePath, ['src/dev-utils.ts']);
+    const content = fs.readFileSync(readmePath, 'utf8');
+    expect(content).to.not.include(PATTERN_STRING);
+    expect(content).to.include('generateApiDocsAndUpdateReadme');
+    expect(content).to.not.include('Static**');
+    expect(content).to.include('**`example`**\n```javascript\n');
   });
 });
